6f9c2157cc63d0319cd234f646223ea89343b32e
Davin <65677163+davin2020@users.noreply.github.com>
2022-01-09 19:43:36 +0000

    removed extra test code functions not being used

--- tests/BoilerPlateTest.php
+++ tests/BoilerPlateTest.php
@@ -20,30 +20,7 @@ class BoilerPlateTest extends TestCase
  
     //function names should match corresponding class method name - names should be descriptive as will get used by --testdox flag
     //test function might not return something, but function itself must, in order to be testable
-    //name must start with word Test + Success or Failure or Malformed + function name, or can be stand alone method eg assertInstanceOf
-
-    // Success Test = pass in good/valid input of correct data type
-    // public function testSuccess_AddSomething(): void
-    // {
-    //     $result = $this->boiler->addSomething(2, 5);
-    //     $this->assertEquals(7, $result);
-    // }
- 
-    // Failure Test = pass in correct data type that is outside of range, to test that this fails as expected (assumes a retricted range of inputs)
-    // public function testFailureExampleNumbersNotEquals(): void {
-    //     $this->assertNotEquals(5, 9);
-    // }
-
-    // Malformed Test = for when you expect an exception/error to be thrown
-    // public function  testMalformedAddingIntToArrayReturnsError()
-    // {
-    //     $inputInt = 77;
-    //     $inputArray = [];
-    //     // must call the expectException() line before the $result line
-    //     $this->expectException(TypeError::class);
-    //     $result = $this->boiler->addSomething($inputInt, $inputArray);
-    // }
-
+    //name must start with word Test + Success (with valid input) or Failure (with incorrect data type or data outside of expected range) or Malformed (when an exception/error is expected) + function name, or can be stand alone method eg assertInstanceOf
 
 
     public function testSuccess1_calculateFinalPosition150(): void
